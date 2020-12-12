2ff16e71463c11bb186d21d753a0aae6272e8acf
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-11 21:07:24 -0800

    added firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyAmfFCM9RKrlIW7SWxMwaxc_J9Bta3nZbg",
+  authDomain: "queso-e7bfe.firebaseapp.com",
+  databaseURL: "https://queso-e7bfe-default-rtdb.firebaseio.com",
+  projectId: "queso-e7bfe",
+  storageBucket: "queso-e7bfe.appspot.com",
+  messagingSenderId: "531114658733",
+  appId: "1:531114658733:web:1e87951f42763491bdef24",
+  measurementId: "G-394NXX88EX",
+};
+
+firebase.initializeApp(config);
