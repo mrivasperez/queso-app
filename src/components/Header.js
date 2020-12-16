800eb868794c377d26ae3a3542f40aa652d0402c
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-16 10:03:36 -0800

    began styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>queso</h1>
-    <NavLink to="/dashboard" activeClassName="is-active" exact={true}>
-      Dashboard{" "}
-    </NavLink>
+  <header className="header">
+    <Link className="header__title" to="/dashboard" exact={true}>
+      <img
+        className="header__title__logo"
+        src="/images/logo.svg"
+        alt="a slice of cheese"
+      />
+      <h1>queso</h1>
+    </Link>
     <NavLink to="/create" activeClassName="is-active" exact={true}>
       Create Expense{" "}
     </NavLink>
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,17 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <img src="/images/logo.svg" alt="A slice of cheese" />
+      <h1 className="box-layout__title"> queso</h1>
+      <p className="box-layout__subtitle">
+        A simple way to manage your expenses.
+      </p>
+      <button className="button__login" onClick={startLogin}>
+        Login with Google
+      </button>
+    </div>
   </div>
 );
 
